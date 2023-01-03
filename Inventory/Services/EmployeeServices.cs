5837f97903552fe2c241d0e07f43a82e43acda46
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-01-03 16:10:50 +0600

    employee services has done

--- Inventory/Services/EmployeeServices.cs
+++ Inventory/Services/EmployeeServices.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Inventory.Data;
+using Inventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Services
+{
+    public class EmployeeServices : IEmployee
+    {
+        private readonly HRMDbContext _context;
+        public EmployeeServices(HRMDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Employee> Create(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task Delete(int id)
+        {
+            var emp = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(emp);
+
+        }
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetById(int id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        public async Task Update(Employee employee)
+        {
+            _context.Entry(employee).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
