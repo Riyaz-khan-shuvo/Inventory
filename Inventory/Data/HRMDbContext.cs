f291b0b2c46880fd1bf3d968346a75525e7150c1
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-01-03 14:42:14 +0600

    inventory connection has done

---
+++ Inventory/Data/HRMDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Inventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Data
+{
+    public class HRMDbContext : DbContext
+    {
+        public HRMDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Inventory/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventory.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
